1d5d64e9b911b2bdae95a845e2ad02ea5853810b
joaaoleite <joaogsleite@gmail.com>
2017-05-31 14:49:55 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+
+IP="fc01::2"
+
+
+
+if [ "$1" = "keys" ]; then
+    cat ~/.ssh/id_rsa.pub | ssh pi@$IP "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys"
+fi
+
+
+if [ "$1" = "code" ]; then
+
+    ssh -6 pi@$IP 'rm -rf ~/rv-project && mkdir rv-project'
+
+    scp -r [!.]* pi@\[$IP\]:rv-project/
+
+fi
